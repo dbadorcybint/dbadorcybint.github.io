925b78e675ad8f4e97447cf02bbe8fbea3976647
dbadorcybint <83402804+dbadorcybint@users.noreply.github.com>
2021-07-16 10:56:24 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,31 +6,13 @@
         updateLtiLaunchForm()
     };
 
-    $('#flexRadioDefault1').click(function() {
-        if (document.getElementById('flexRadioDefault1').classList.contains('selected-check')) {
-            $('#flexRadioDefault1').removeClass('selected-check')
-        } else {
-            $('#flexRadioDefault1').addClass('selected-check')
-        }
-        if (!document.getElementById('flexRadioDefault1').classList.contains('selected-check')) {
-            $('#flexRadioDefault1').prop('checked', false)
-        } else {
-            $('#flexRadioDefault1').prop('checked', true)
-        }
-    })
-
     this.updateLtiLaunchForm = function () {
         var launchUrl = $('#launchUrl').val();
-        if (sessionStorage.getItem('secret') !== null) {
-            var encodedSecret = encodeURIComponent($('#secret').val()) + '&';
-        }
-
+        var encodedSecret = encodeURIComponent($('#secret').val()) + '&';
         var $ltiLaunchForm = $('#ltiLaunchForm');
 
         $ltiLaunchForm.attr('action', launchUrl);
-        if (sessionStorage.getItem('key') !== null) {
-            $('#oauth_consumer_key').val($('#key').val());
-        }
+        $('#oauth_consumer_key').val($('#key').val());
         $('#oauth_timestamp').val(Math.floor(new Date().getTime() / 1000));
         $('#oauth_nonce').val(uniqid('', true));
 
@@ -119,3 +101,93 @@
         return result;
     };
 })();
+
+const loginBtn = document.getElementById('loginBtn')
+
+const loginColumn = document.getElementById('loginColumn')
+const leftColumn = document.getElementById('leftColumn')
+
+const part1 = document.getElementById('part-1')
+const part2 = document.getElementById('part-2')
+const part3 = document.getElementById('part-3')
+const part4 = document.getElementById('part-4')
+const part5 = document.getElementById('part-5')
+
+const next1 = document.getElementById('next-1')
+const next2 = document.getElementById('next-2')
+const next3 = document.getElementById('next-3')
+const next4 = document.getElementById('next-4')
+
+const launch = document.getElementById('launch_lti')
+
+const back1 = document.getElementById('back-1')
+const back2 = document.getElementById('back-2')
+const back3 = document.getElementById('back-3')
+const back4 = document.getElementById('back-4')
+
+const progressZero = document.getElementById('progressZero')
+const progressTwentyFive = document.getElementById('progressTwentyFive')
+const progressFifty = document.getElementById('progressFifty')
+const progressSeventyFive = document.getElementById('progressSeventyFive')
+const progressOneHundred = document.getElementById('progressOneHundred')
+
+loginBtn.addEventListener('click', function () {
+    loginColumn.classList.add('hide-par')
+    leftColumn.classList.remove('hide-par')
+})
+
+next1.addEventListener('click', function () {
+    part1.classList.add('hide-par')
+    part2.classList.remove('hide-par')
+    progressZero.classList.add('hide-progress')
+    progressTwentyFive.classList.remove('hide-progress')
+})
+
+next2.addEventListener('click', function () {
+    part2.classList.add('hide-par')
+    part3.classList.remove('hide-par')
+    progressTwentyFive.classList.add('hide-progress')
+    progressFifty.classList.remove('hide-progress')
+})
+
+next3.addEventListener('click', function () {
+    part3.classList.add('hide-par')
+    part4.classList.remove('hide-par')
+    progressFifty.classList.add('hide-progress')
+    progressSeventyFive.classList.remove('hide-progress')
+})
+
+next4.addEventListener('click', function () {
+    part4.classList.add('hide-par')
+    part5.classList.remove('hide-par')
+    progressSeventyFive.classList.add('hide-progress')
+    progressOneHundred.classList.remove('hide-progress')
+})
+
+back1.addEventListener('click', function () {
+    part2.classList.add('hide-par')
+    part1.classList.remove('hide-par')
+    progressTwentyFive.classList.add('hide-progress')
+    progressZero.classList.remove('hide-progress')
+})
+
+back2.addEventListener('click', function () {
+    part3.classList.add('hide-par')
+    part2.classList.remove('hide-par')
+    progressFifty.classList.add('hide-progress')
+    progressTwentyFive.classList.remove('hide-progress')
+})
+
+back3.addEventListener('click', function () {
+    part4.classList.add('hide-par')
+    part3.classList.remove('hide-par')
+    progressSeventyFive.classList.add('hide-progress')
+    progressFifty.classList.remove('hide-progress')
+})
+
+back4.addEventListener('click', function () {
+    part5.classList.add('hide-par')
+    part4.classList.remove('hide-par')
+    progressOneHundred.classList.add('hide-progress')
+    progressSeventyFive.classList.remove('hide-progress')
+})
